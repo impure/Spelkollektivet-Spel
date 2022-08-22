24a18105dea65c43574f98dd8d64e5248c7aa0b7
Andrew Zuo <andrew@andrewzuo.com>
2022-08-22 18:10:30 +0200

    Now we can start and end rounds

--- lib/GameState.dart
+++ lib/GameState.dart
@@ -1,12 +1,57 @@
 
+import 'dart:math';
+
 import 'package:spelkollektivet_spel/Card.dart';
 import 'package:tools/RandomBag.dart';
+
+Random rng = Random();
 
 RandomBag<Card> cardPool = RandomBag<Card>();
 RandomBag<Card> cardPoolDiscards = RandomBag<Card>();
 
-RandomBag<ItemCard> itemBool = RandomBag<ItemCard>();
+RandomBag<ItemCard> itemPool = RandomBag<ItemCard>();
 RandomBag<ItemCard> itemDiscards = RandomBag<ItemCard>();
+
+List<Card> cardsCanBeBought = <Card>[];
+List<ItemCard> itemsCanBeBought = <ItemCard>[];
+
+void startRound() {
+  assert(cardsCanBeBought.isEmpty);
+  assert(itemsCanBeBought.isEmpty);
+
+  for (int i = 0; i < 4; i++) {
+    if (cardPool.empty) {
+      cardPool = cardPoolDiscards;
+      cardPoolDiscards = RandomBag<Card>();
+      // TODO: end game if ran out of cards
+    }
+    cardsCanBeBought.add(cardPool.getRandomItem(rng));
+  }
+
+  for (int i = 0; i < 12; i++) {
+    if (itemPool.empty) {
+      itemPool = itemDiscards;
+      itemDiscards = RandomBag<ItemCard>();
+      if (itemPool.empty) {
+        return;
+      }
+    }
+    itemsCanBeBought.add(itemPool.getRandomItem(rng));
+  }
+}
+
+void endRound() {
+
+  for (int i = 0; i < cardsCanBeBought.length; i++) {
+    cardPoolDiscards.add(cardsCanBeBought[i]);
+  }
+  cardsCanBeBought.clear();
+
+  for (int i = 0; i < itemsCanBeBought.length; i++) {
+    itemDiscards.add(itemsCanBeBought[i]);
+  }
+  itemsCanBeBought.clear();
+}
 
 void initGameState() {
   cardPool.add(Card.HouseMeeting());
@@ -25,4 +70,6 @@ void initGameState() {
   cardPool.add(Card.StandUp());
   cardPool.add(Card.StandUp());
   cardPool.add(Card.StandUp());
+
+  startRound();
 }
